ddd8d3354b42d4058864ab193a4034693fae3016
Giulio_Pa <giuliop93@hotmail.it>
2021-05-03 10:17:12 +0200

    wip

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 //inizializzo Vue
-var app = new Vue(
+const app = new Vue(
     {
         el: '#vueContainer',
        data: {
             message:"test",
-            images:["images/Uno.jpg"]
+            var: 0,
+            images:["images/Uno.jpg", "images/due.jpg", "images/tre.jpg", "images/quattro.jpg"]
+        },
+        methods: {
+            /**
+             * 
+             * @param {number}}direction - +1 se voglio andare avanti, -1 se indeitro direction 
+             */
+            changeImg(direction){
+                let newIndex = this.activeImg += direction;
+se 
+                if(newIndex < 0){
+                    // se l'indice < di 0 facciamo andare l'utente all'ultima immagine della lista
+                    newIndex = this.images.length
+                } else if (newIndex > (this.images.length - 1)){
+                    // se l'indice è già l'ultimo della lista delle immagini, facciamo andare l'utente alla prima immagine
+                    newIndex = 0;
+                    this.activeImg = 0
+                }
+
+                this.activeImg = newIndex;
+            }
         }
     }
 )
